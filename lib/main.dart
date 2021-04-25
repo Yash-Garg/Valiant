46cc3d2bf4b7934f07f03ebf4bd1c8c0f2e361c0
Yash-Garg <yash.garg@outlook.in>
2021-04-25 21:58:06 +0530

    Add screens and fix logo
    Signed-off-by: Yash-Garg <yash.garg@outlook.in>

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:valiant/pages/home_page.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:valiant/pages/home_screen.dart';
+import 'package:valiant/pages/splash_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Valiant',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: HomeScreen(),
+      initialRoute: "/splash",
+      getPages: [
+        GetPage(
+          name: "/splash",
+          page: () => SplashScreen(),
+          transition: Transition.leftToRightWithFade,
+        ),
+        GetPage(
+          name: "/home",
+          page: () => HomeScreen(),
+        ),
+      ],
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        fontFamily: GoogleFonts.openSans().fontFamily,
+        appBarTheme: AppBarTheme(color: Colors.transparent),
+        scaffoldBackgroundColor: Colors.black,
+      ),
     );
   }
 }
